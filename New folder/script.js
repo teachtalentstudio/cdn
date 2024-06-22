6048c161d78e120b80e483f3d84a7952e14d4e0d
teachtalentstudio <teachtalentstudio@gmail.com>
2024-06-22 21:43:47 +0500

    Add files via upload

---
+++ New folder/script.js
@@ -0,0 +1,487 @@
+
+
+
+    
+const Fabrication: {
+        Lesson1: {
+            Lesson_heading: {
+                title: 'Basic Learning'
+            },
+            Lesson_Cols: {
+                First_Col: {
+                    Col_heading: {
+                        title: 'Tools'
+                    },
+                    Unordered_list: {
+                        first_li: {
+                            li: 'Meter Tape',
+                            li_content: ``
+                        },
+                        second_li: {
+                            li: 'Hammer',
+                            li_content: '<img src='https://i.imgur.com/lm6nUpK.png'/>'
+                        },
+                        third_li: {
+                            li: 'Marker',
+                            li_content: '
+                        },
+                        fourth_li: {
+                            li: 'Drill Machine',
+                            li_content: '
+                        },
+                        fifth_li: {
+                            li: 'Grander 4\'5\'7\'9\',
+                            li_content: '
+                        },
+                        sixth_li: {
+                            li: 'Gas Cutting Set',
+                            li_content: '
+                        },
+                        seventh_li: {
+                            li: 'Center Punch',
+                            li_content: '
+                        },
+                        eighth_li: {
+                            li: 'Right Angle',
+                            li_content: '
+                        },
+                        ninth_li: {
+                            li: 'Spirit Level / Hand Level',
+                            li_content: '
+                        },
+                        tenth_li: {
+                            li: 'Spanner Set',
+                            li_content: '
+                        },
+                        eleventh_li: {
+                            li: 'Sparking Lighter',
+                            li_content: '
+                        },
+                        twelfth_li: {
+                            li: 'Welding Machines',
+                            li_content: '
+                        },
+                        thirteenth_li: {
+                            li: 'Pencil Grander',
+                            li_content: '
+                        },
+                        fourteenth_li: {
+                            li: 'Adjustable',
+                            li_content: '
+                        },
+                        fifteenth_li: {
+                            li: 'Cylinder Key',
+                            li_content: '
+                        },
+                        sixteenth_li: {
+                            li: 'Oxygen Cylinder',
+                            li_content: '
+                        },
+                        seventeenth_li: {
+                            li: 'LPG Cylinder',
+                            li_content: '
+                        },
+                        eighteenth_li: {
+                            li: 'TurnBuckle',
+                            li_content: '
+                        },
+                        nineteenth_li: {
+                            li: 'Block Chain',
+                            li_content: '
+                        },
+                        twentieth_li: {
+                            li: 'Plumb Bob',
+                            li_content: '
+                        },
+                        twenty_first_li: {
+                            li: 'Line Dori',
+                            li_content: '
+                        },
+                        twenty_second_li: {
+                            li: 'Teen Cutter',
+                            li_content: '
+                        },
+                        twenty_third_li: {
+                            li: 'Files and Kind',
+                            li_content: '
+                        },
+                        twenty_fourth_li: {
+                            li: 'Scale 2m _ 6m',
+                            li_content: '
+                        },
+                        twenty_fifth_li: {
+                            li: 'Pipe Vise',
+                            li_content: '
+                        },
+                        twenty_sixth_li: {
+                            li: 'Banch Vise',
+                            li_content: '
+                        },
+                        twenty_seventh_li: {
+                            li: 'Hexa',
+                            li_content: '
+                        },
+                        twenty_eighth_li: {
+                            li: 'Chisles',
+                            li_content: '
+                        }
+                    }
+                    
+                },
+                Second_Col: {
+                    Col_heading: {
+                        title: 'Beam'
+                    },
+                    Unordered_list: {
+                        first_li: {
+                            li: 'Beam',
+                            li_content: '
+                        },
+                        second_li: {
+                            li: 'H Beam',
+                            li_content: '
+                        },
+                        third_li: {
+                            li: 'I Beam',
+                            li_content: '
+                        },
+                        fourth_li: {
+                            li: 'C Channel',
+                            li_content: '
+                        },
+                        fifth_li: {
+                            li: 'Angle',
+                            li_content: '
+                        },
+                        sixth_li: {
+                            li: 'Equal Angle',
+                            li_content: '
+                        },
+                        seventh_li: {
+                            li: 'Non Equal Angle',
+                            li_content: '
+                        },
+                        eighth_li: {
+                            li: 'MS(Mild Steel) Plate',
+                            li_content: '
+                        },
+                        ninth_li: {
+                            li: 'Flat Bar',
+                            li_content: '
+                        },
+                        tenth_li: {
+                            li: 'Round Bar',
+                            li_content: '
+                        },
+                        eleventh_li: {
+                            li: 'Chackered Plate',
+                            li_content: '
+                        },
+                        twelfth_li: {
+                            li: 'T iran',
+                            li_content: '
+                        },
+                        thirteenth_li: {
+                            li: 'Pipe',
+                            li_content: '
+                        },
+                        fourteenth_li: {
+                            li: 'Square Pipe',
+                            li_content: '
+                        },
+                        fifteenth_li: {
+                            li: 'Rectangular Pipe',
+                            li_content: '
+                        }
+                    }
+                    
+            },
+            Third_Col: {
+                Col_heading: {
+                    title: 'Piping Material'
+                },
+                Unordered_list: {
+                    first_li: {
+                        li: 'Pipe',
+                        li_content: '
+                    },
+                    second_li: {
+                        li: 'Elbow',
+                        li_content: '
+                    },
+                    third_li: {
+                        li: 'Reducer',
+                        li_content: '
+                    },
+                    fourth_li: {
+                        li: 'Socket',
+                        li_content: '
+                    },
+                    fifth_li: {
+                        li: 'Socket Tee',
+                        li_content: '
+                    },
+                    sixth_li: {
+                        li: 'End Cap Plug',
+                        li_content: '
+                    },
+                    seventh_li: {
+                        li: 'Gas Kit',
+                        li_content: '
+                    },
+                    eighth_li: {
+                        li: 'Flange',
+                        li_content: '
+                    },
+                    ninth_li: {
+                        li: 'Plastic Pipe',
+                        li_content: '
+                    },
+                    tenth_li: {
+                        li: 'PVC(Polyvinyl Chloride) Pipe',
+                        li_content: '
+                    },
+                    eleventh_li: {
+                        li: 'Valves',
+                        li_content: '
+                    },
+                    twelfth_li: {
+                        li: 'Spanner Set',
+                        li_content: '
+                    },
+                    thirteenth_li: {
+                        li: 'Open Spanner',
+                        li_content: '
+                    },
+                    fourteenth_li: {
+                        li: 'Ring Spanner',
+                        li_content: '
+                    },
+                    fifteenth_li: {
+                        li: 'Combinator Spanner',
+                        li_content: '
+                    },
+                    sixteenth_li: {
+                        li: 'Striking Spanner',
+                        li_content: '
+                    }
+                }
+                
+            },
+            Fourth_Col: {
+                Col_heading: {
+                    title: 'Gas Cutting Set'
+                },
+                Unordered_list: {
+                    first_li: {
+                        li: 'Cutting Torch',
+                        li_content: '
+                    },
+                    second_li: {
+                        li: 'Tip Nosel',
+                        li_content: '
+                    },
+                    third_li: {
+                        li: 'Hose Pipe (red &amp; blue)',
+                        li_content: '
+                    },
+                    fourth_li: {
+                        li: 'Oxygen Cylinder',
+                        li_content: '
+                    },
+                    fifth_li: {
+                        li: 'Cylinder Key',
+                        li_content: '
+                    },
+                    sixth_li: {
+                        li: 'LPG Gas (Fon Gas)',
+                        li_content: '
+                    },
+                    seventh_li: {
+                        li: 'Adjustable',
+                        li_content: '
+                    },
+                    eighth_li: {
+                        li: 'Tip Cleaner',
+                        li_content: '
+                    },
+                    ninth_li: {
+                        li: 'Flash Back Arrestor',
+                        li_content: '
+                    },
+                    tenth_li: {
+                        li: 'Screw Driver',
+                        li_content: '
+                    }
+                }
+                
+            },
+            Fifth_col: {
+                Col_heading: {
+                    title: 'Flange Content'
+                },
+                Unordered_list: {
+                    first_li: {
+                        li: 'Slip on Flange',
+                        li_content: '
+                    },
+                    second_li: {
+                        li: 'Socket Flange',
+                        li_content: '
+                    },
+                    third_li: {
+                        li: 'Weld Neck Flange',
+                        li_content: '
+                    },
+                    fourth_li: {
+                        li: 'Oriface Flange',
+                        li_content: '
+                    },
+                    fifth_li: {
+                        li: 'Oriface Flange',
+                        li_content: '
+                    },
+                    sixth_li: {
+                        li: 'Male Female Flange',
+                        li_content: '
+                    },
+                    seventh_li: {
+                        li: 'Threaded Flange',
+                        li_content: '
+                    },
+                    eighth_li: {
+                        li: 'Raised face flange',
+                        li_content: '
+                    },
+                    ninth_li: {
+                        li: 'Mechanical Flange',
+                        li_content: '
+                    }
+                }
+                
+            },
+            Sixth_Col: {
+                Col_heading: {
+                    title: 'Valves'
+                },
+                Unordered_list: {
+                    first_li: {
+                        li: 'Non Return Flange',
+                        li_content: '
+                    },
+                    second_li: {
+                        li: 'Gate Valve',
+                        li_content: '
+                    },
+                    third_li: {
+                        li: 'Globe Valve',
+                        li_content: '
+                    },
+                    fourth_li: {
+                        li: 'Ball Valve',
+                        li_content: '
+                    },
+                    fifth_li: {
+                        li: 'Needle Valve',
+                        li_content: '
+                    },
+                    sixth_li: {
+                        li: 'Control Valve',
+                        li_content: '
+                    },
+                    seventh_li: {
+                        li: 'Safety Valve',
+                        li_content: '
+                    },
+                    eighth_li: {
+                        li: 'Three way valve',
+                        li_content: '
+                    },
+                    ninth_li: {
+                        li: 'Four way Valve',
+                        li_content: '
+                    },
+                    tenth_li: {
+                        li: 'Butterfly Valve',
+                        li_content: '
+                    },
+                    eleventh_li: {
+                        li: 'Angle Valve',
+                        li_content: '
+                    }
+                }
+                
+            }
+
+        }
+
+
+    }
+    
+    /*
+     class_name: {
+        Lesson1: {
+            Lesson_heading: {
+                Title: ',
+            },
+            Lesson_Cols: {
+                First_Col: {
+                    Col_heading: {
+                        title: '
+                    },
+                    Unordered_list: {
+                        first_li: {
+                            li: ',
+                            li_content: '
+                        },
+                        
+                    }
+                    
+                },
+                Second_Col: {
+                    
+                    
+            },
+            
+
+        }
+
+
+    },
+},
+    
+    */
+
+const modal = document.querySelector('.modal');
+    
+
+        for (let lesson in postInformation[sectionName]) {
+            postContainer.appendChild(document.createElement('h1')).innerHTML = `${postInformation[sectionName][lesson].Lesson_heading.title}`;
+
+
+            for(let lessonCols in postInformation[sectionName][lesson].Lesson_Cols) {
+                let createCard = postContainer.appendChild(document.createElement('div'));
+                createCard.classList.add('card');
+                createCard.innerHTML = `<h2>${postInformation[sectionName][lesson].Lesson_Cols[lessonCols].Col_heading.title}</h2>`;
+
+
+                let createul = createCard.appendChild(document.createElement('ul'));
+
+                for (let Countli in postInformation[sectionName][lesson].Lesson_Cols[lessonCols].Unordered_list) {
+                    let liClick = createul.appendChild(document.createElement('li'));
+                    liClick.innerHTML = `${postInformation[sectionName][lesson].Lesson_Cols[lessonCols].Unordered_list[Countli].li}`;
+
+                    liClick.addEventListener('click', () =&gt; {
+                        modal.style.display = 'block';
+                        modal.innerHTML = `<span onclick='this.parentNode.style.display =&apos;none&apos;'>&#10062;</span>${postInformation[sectionName][lesson].Lesson_Cols[lessonCols].Unordered_list[Countli].li_content}`;
+                    })
+
+                }
+            }
+        }
+    }
+
+
+
+    
+  
